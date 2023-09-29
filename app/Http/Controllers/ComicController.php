f99cb8e3c83a9a687198ebb422f7d82ca3855c95
ColluraV <valerio.18.02.89@hotmail.it>
2023-09-29 16:12:36 +0200

    errore da fixare nel foreach

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class ComicController extends Controller
@@ -11,7 +12,9 @@ class ComicController extends Controller
      */
     public function index()
     {
-        //
+        $comicsList = Comic::all();
+        
+        return view('index',["comics"=>$comicsList]);
     }
 
     /**
